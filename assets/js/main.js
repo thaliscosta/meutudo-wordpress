eb31c3e1d08424d7adb7393363de8463d8304e8c
Thalis Costa <thalis@agita.com.br>
2023-04-16 12:17:13 -0300

    Fim do frontend

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 AOS.init();
 
+document.querySelector('[js-toggle-menu]').addEventListener('click', function(e) {
+    e.preventDefault();
+    this.classList.toggle('active');
+    document.querySelector('nav').classList.toggle('active');
+});
+
 const header = document.querySelector('header').offsetTop;
 window.addEventListener('scroll', function () {
     if (window.pageYOffset > header) {
@@ -7,4 +13,11 @@ window.addEventListener('scroll', function () {
     } else {
         document.querySelector('header').classList.remove('white-header');
     }
+});
+
+
+const swiperLoans = new Swiper(".swiper-loans", {
+  pagination: {
+    el: ".swiper-pagination",
+},
 });
